55d3f0ff950c8dc8267953087f65765b029cac02
Daniel Kamel <34145238+daniel-kamel@users.noreply.github.com>
2023-10-06 09:03:40 -0700

    Create 9-print_comb

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: prints all the letters of the alphabet
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i = 48;
+
+	while (i < 58)
+	{
+		putchar(i);
+
+		if (i < 57)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+
+		i++;
+	}
+
+	putchar('\n');
+
+	return (0);
+}
