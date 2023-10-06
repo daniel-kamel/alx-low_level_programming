205c44ff99b655d6daef137afb34e25f1c26fe48
Daniel Kamel <34145238+daniel-kamel@users.noreply.github.com>
2023-10-06 08:32:24 -0700

    Create 3-print_alphabets.c

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: prints all the letters of the alphabet
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i = 97;
+
+	while (i < 123)
+	{
+		putchar(i);
+		i++;
+	}
+
+	i = 65;
+
+	while (i < 91)
+	{
+		putchar(i);
+                i++;
+        }
+
+	putchar('\n');
+
+	return (0);
+}
