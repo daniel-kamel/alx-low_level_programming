19806195c590194c886ea7a6e0f7634f77c442b6
Daniel Kamel <34145238+daniel-kamel@users.noreply.github.com>
2023-10-10 08:09:12 -0700

    Create 0-putchar.c

--- 0x02-functions_nested_loops/_putchar.c
+++ 0x02-functions_nested_loops/_putchar.c
@@ -11,23 +11,3 @@ int _putchar(char c)
 {
 	return (write(1, &c, 1));
 }
-
-/**
- * main - prints _putchar
- *
- * Return: Always 0 (Success)
- */
-int main(void)
-{
-	char *s = "_putchar";
-	int i = 0;
-
-	while (i < 10)
-	{
-		_putchar(s[i]);
-		i++;
-	}
-	_putchar('\n');
-
-	return (0);
-}
