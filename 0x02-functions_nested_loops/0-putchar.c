be41efbf46275d0149f5ce4a4865f74c6a78acf5
Daniel Kamel <34145238+daniel-kamel@users.noreply.github.com>
2023-10-10 07:26:17 -0700

    Create 0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,15 +1,19 @@
 #include <stdio.h>
 #include <string.h>
 
+/**
+ * main - prints _putchar
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
 	char *s = "_putchar";
-	
+
 	for (int i = 0; i < strlen(s); i++)
 	{
 		putchar(s[i]);
 	}
-
 	putchar('\n');
 
 	return (0);
