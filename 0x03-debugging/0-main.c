7017cb33733695ea9bf81d69560db31703d8794c
Daniel Kamel <34145238+daniel-kamel@users.noreply.github.com>
2023-10-11 10:05:00 -0700

    Create 0-main.c

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - entry point
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	int i = 0;
+
+	positive_or_negative(i);
+
+	return (0);
+}
