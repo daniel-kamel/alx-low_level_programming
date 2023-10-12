a900f993a1d06546221e049c80ae41192db749d7
Daniel Kamel <34145238+daniel-kamel@users.noreply.github.com>
2023-10-12 15:12:19 +0300

    Create 3-print_numbers.c

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -7,12 +7,12 @@
  */
 void print_numbers(void)
 {
-    char c = '0';
+	char c = '0';
 
-    while (c <= '9')
-    {
-        _putchar(c);
-        c++;
-    }
-    _putchar('\n');
+	while (c <= '9')
+	{
+		_putchar(c);
+		c++;
+	}
+	_putchar('\n');
 }
