29a1b7ac715664f3b38c27e61c421c680b1ecd0d
Daniel Kamel <34145238+daniel-kamel@users.noreply.github.com>
2023-10-17 00:45:57 +0300

    Create 9-strcpy.c

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ *   _strcpy - copies string into another
+ *  @dest: pointer to first char in destination string
+ *  @src: pointer to first char in src string
+ *  Return: pointer to destination
+ */
+char *_strcpy(char *dest, char *src)
+{
+	int i = 0;
+
+	while (src[i] != '\0')
+	{
+		dest[i] = src[i];
+		i++;
+	}
+
+	dest[i] = '\0';
+	
+	return (dest);
+}
