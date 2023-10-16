0bdd757d65dfecf0d37443f9252155e087d5785b
Daniel Kamel <34145238+daniel-kamel@users.noreply.github.com>
2023-10-17 00:29:21 +0300

    Create 7-puts_half.c

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * puts2 - prints every other character
+ * @str: string
+ * Return: void
+*/
+void puts_half(char *str)
+{
+	int i = 0;
+
+	while (*str != '\0')
+	{
+		i++;
+		str++;
+	}
+	i = i / 2;
+	while (i)
+	{
+		str--;
+		i--;
+	}
+
+	while (*str != '\0')
+	{
+		_putchar(*str);
+		str++;
+	}
+
+	_putchar('\n');
+}
