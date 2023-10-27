a0ff5c31d83510b2e3770e7606601847205f8a29
Daniel Kamel <34145238+daniel-kamel@users.noreply.github.com>
2023-10-27 19:20:02 +0200

    Create 0-whatsmyname.c

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - prints the program's name
+ * @argc: number of command-line arguments
+ * @argv: CL argumanets array
+ * Return: Always 0 (success)
+*/
+int main(int  __attribute__((__unused__)) argc, char *argv[])
+{
+	printf("%s\n", argv[0]);
+
+	return (0);
+}
