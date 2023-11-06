ac8a5e6f9558f32b4c711708f41517031241edc4
Daniel Kamel <34145238+daniel-kamel@users.noreply.github.com>
2023-11-07 01:17:03 +0200

    Create dog.h

---
+++ 0x0E-structures_typedef/README.md
@@ -0,0 +1,1 @@
+This directory contains answers to the tasks of 0x0E-structures_typedef project in ALX SE program
---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,17 @@
+#ifndef STRUCT_DOG
+
+#define STRUCT_DOG
+/**
+ * struct dog - dog data structer
+ * @name: dog's name
+ * @age: dog's age
+ * @owner: dog's owner
+*/
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
