91480aa4411db2411ee61168a14ece0c18321d75
Daniel Kamel <34145238+daniel-kamel@users.noreply.github.com>
2023-11-07 02:07:14 +0200

    Create 4-new_dog.c

---
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "dog.h"
+
+/**
+ * new_dog - creates a new dog
+ * @name: name
+ * @age: age
+ * @owner: owner
+ * Return: new dog
+*/
+dog_t *new_dog(char *name, float age, char *owner)
+{
+	dog_t *woof = malloc(sizeof(dog_t));
+
+	if (!woof || !name || age < 0 || !owner)
+		return (NULL);
+
+	woof->name = malloc(sizeof(char) * strlen(name) + 1);
+	if (!woof->name)
+		return (NULL);
+
+	woof->owner = malloc(sizeof(char) * strlen(owner) + 1);
+	if (!woof->owner)
+		return (NULL);
+
+	strcpy(woof->name, name);
+	woof->age = age;
+	strcpy(woof->owner, owner);
+
+	return (woof);
+}
