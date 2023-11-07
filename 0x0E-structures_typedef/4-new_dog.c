2ad045b8dc3733b89e3497b9d93466876b64cd8f
Daniel Kamel <34145238+daniel-kamel@users.noreply.github.com>
2023-11-07 02:10:16 +0200

    Create 4-new_dog.c

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -15,18 +15,24 @@ dog_t *new_dog(char *name, float age, char *owner)
 	dog_t *woof = malloc(sizeof(dog_t));
 
 	if (!woof || !name || age < 0 || !owner)
+	{
 		free(woof);
 		return (NULL);
+	}
 
 	woof->name = malloc(sizeof(char) * strlen(name) + 1);
 	if (!woof->name)
+	{
 		free(woof);
 		return (NULL);
+	}
 
 	woof->owner = malloc(sizeof(char) * strlen(owner) + 1);
 	if (!woof->owner)
+	{
 		free(woof);
 		return (NULL);
+	}
 
 	strcpy(woof->name, name);
 	woof->age = age;
