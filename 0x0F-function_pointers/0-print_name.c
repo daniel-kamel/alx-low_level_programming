8be2a4e60895734899afd08eb711ad70c8a5ce07
Daniel Kamel <34145238+daniel-kamel@users.noreply.github.com>
2023-11-08 20:23:48 +0200

    Create 0-print_name.c

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,6 +1,4 @@
 #include "function_pointers.h"
-#include <stdio.h>
-#include <stdlib.h>
 
 /**
  * print_name - prints name
@@ -10,8 +8,6 @@
 */
 void print_name(char *name, void (*f)(char *))
 {
-	if (!f || !name)
-		exit(1);
-
-	f(name);
+	if (f && name)
+		f(name);
 }
