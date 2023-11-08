53b5044d84da3adb60854e36f3dcc1a08fa5b09b
Daniel Kamel <34145238+daniel-kamel@users.noreply.github.com>
2023-11-08 20:49:59 +0200

    Create 2-int_index.c

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,27 @@
+#include "function_pointers.h"
+
+/**
+ * int_index - searches an array
+ * @array: array
+ * @size: size of array
+ * @cmp: compare function
+ * Return: first occurance of element or -1 if not found
+*/
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int i;
+
+	if (!array || !cmp)
+		exit(1);
+
+	if (size <= 0)
+		return (-1);
+
+	for (i = 0; i < size; i++)
+	{
+		if (cmp(array[i]) != 0)
+			return (i);
+	}
+
+	return (-1);
+}
