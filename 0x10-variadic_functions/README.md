a20e24c7fb3f8cafae43042af32548b93343e155
Daniel Kamel <34145238+daniel-kamel@users.noreply.github.com>
2023-11-09 16:29:42 +0200

    Create 0-sum_them_all.c

---
+++ 0x10-variadic_functions/README.md
@@ -0,0 +1,1 @@
+This directory contains answers to the tasks of 0x10-variadic_functions project in ALX SE program
---
+++ 0x10-variadic_functions/variadic_functions.h
@@ -0,0 +1,9 @@
+#ifndef MAIN
+
+#define MAIN
+#include <stdarg.h>
+#include <stdio.h>
+#include <stdlib.h>
+int sum_them_all(const unsigned int n, ...);
+
+#endif
