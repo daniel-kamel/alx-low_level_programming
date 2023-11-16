490300936e147419b06b40022ff5ba313fcb8e53
Daniel Kamel <34145238+daniel-kamel@users.noreply.github.com>
2023-11-16 21:57:31 +0200

    Create 4-free_list.c

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,25 @@
+#include "lists.h"
+
+/**
+ * free_list - frees list memory
+ * @head: address of list head
+ * Return: void
+*/
+void free_list(list_t *head)
+{
+	list_t *current = head;
+
+	if (!head)
+		exit(-1);
+
+	while (current)
+	{
+		current = current->next;
+
+		if (head->str)
+			free(head->str);
+		free(head);
+
+		head = current;
+	}
+}
