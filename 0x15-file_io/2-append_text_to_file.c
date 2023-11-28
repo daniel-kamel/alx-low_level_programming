cd7f7f572d291b7b07119e43de6f69629aca09b9
Daniel Kamel <34145238+daniel-kamel@users.noreply.github.com>
2023-11-28 03:08:52 +0200

    Create 2-append_text_to_file.c

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -1,15 +1,33 @@
 #include "main.h"
 
 /**
+ * _strlen - returns the length of a string
+ * @s: the string whose length to check
+ *
+ * Return: integer length of string
+*/
+int _strlen(char *s)
+{
+	int i = 0;
+
+	if (!s)
+		return (0);
+	while (*s++)
+		i++;
+	return (i);
+}
+
+/**
  * append_text_to_file - appends text to a file
- * @filename: file to be appended to
- * @text_content: text to be appended
- * Return: 1 on success or -1 on failiure
+ * @filename: name of file to create
+ * @text_content: text to write
+ *
+ * Return: 1 on success 0 on failure
 */
 int append_text_to_file(const char *filename, char *text_content)
 {
 	int fd;
-	ssize_t bytes = 0, len = strlen(text_content);
+	ssize_t bytes = 0, len = _strlen(text_content);
 
 	if (!filename)
 		return (-1);
