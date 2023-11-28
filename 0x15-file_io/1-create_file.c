f75649a4940760cec6331bb4f8f5859f1e6ac410
Daniel Kamel <34145238+daniel-kamel@users.noreply.github.com>
2023-11-28 02:46:43 +0200

    Create 1-create_file.c

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,35 @@
+#include "main.h"
+
+/**
+ * create_file - creates a file
+ * @filename: file to be created
+ * @text_content: contents of the file
+ * Return: 1 on success or -1 on failiure
+*/
+int create_file(const char *filename, char *text_content)
+{
+	int fd;
+	ssize_t len = strlen(text_content), written = 0;
+
+	if (!filename)
+		return (-1);
+
+	fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0600);
+	if (fd == -1)
+		return (-1);
+
+	if (!text_content)
+	{
+		write(fd, "", 0);
+		close(fd);
+		return (1);
+	}
+
+	written = write(fd, text_content, len);
+	close(fd);
+
+	if (written != len)
+		return (-1);
+
+	return (1);
+}
