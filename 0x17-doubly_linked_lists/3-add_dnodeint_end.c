06ea40b13fadc633ad70c1fabebfc19f64c6b73d
Daniel Kamel <34145238+daniel-kamel@users.noreply.github.com>
2024-01-11 23:50:21 +0200

    Create 3-add_dnodeint_end.c

--- 0x17-doubly_linked_lists/3-add_dnodeint_end.c
+++ 0x17-doubly_linked_lists/3-add_dnodeint_end.c
@@ -18,7 +18,7 @@ dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
 	new_node->n = n;
 	new_node->next = NULL;
 
-	if (!*head)
+	if (*head == NULL)
 	{
 		new_node->prev = NULL;
 		*head = new_node;
